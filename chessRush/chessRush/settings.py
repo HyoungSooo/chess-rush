00740c0424691adcffc105643d7907b74f7acdcd
Hyoungsooo <aaa57403@gmail.com>
2023-05-20 13:49:10 +0900

    change settings for mine error

--- chessRush/chessRush/settings.py
+++ chessRush/chessRush/settings.py
@@ -15,6 +15,7 @@ import os
 from datetime import timedelta
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
